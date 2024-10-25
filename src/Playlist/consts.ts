05fe9117dbd239b02fe4d0bc2621f8e1c7dfeeea
juankm08-a11y <juancarlospabon01@gmail.com>
2024-10-25 15:53:35 -0500

    Initial commit

---
+++ src/Playlist/consts.ts
@@ -0,0 +1,20 @@
+import { Track } from '../libs/player';
+
+export const tracks: Track[] = [
+  {
+    title: 'Fix you',
+    url: '/music/Fixyou.mp3',
+  },
+  {
+    title: 'Secrets',
+    url: '/music/Secrets.mp3',
+  },
+  {
+    title: 'TodolobuenoTarda',
+    url: '/music/TodolobuenoTarda.mp3',
+  },
+  {
+    title: 'Sample Video',
+    url: '/videos/sample-video.mp4',
+  },
+];
